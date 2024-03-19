6027c8e48c3dfb8518a781cbfa31385295b922b9
Jolx77 <joaquin.otalora@mi.unc.edu.ar>
2024-03-19 15:58:40 -0300

    Se corrige un error ortografico y se FIRMA el commit

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
-    printf("Holis, bienvenios al himalaia \n");
+    printf("Hola, bienvenios al himalaia \n");
     greetings();
     printf("Llamando a la libreria dinamica desde el main, obtengo '%d'\n", shared_lib_function("Hola, SOP2"));
     printf("Llamando a la libreria dinamica desde el main, obtengo '%d'\n", static_lib_function("Chau, SOP2"));
