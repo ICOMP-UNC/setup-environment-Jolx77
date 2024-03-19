4de569236bcdbb9f1631f694de40975fec3d02c8
Jolx77 <joaquin.otalora@mi.unc.edu.ar>
2024-03-19 11:56:59 -0300

    CL:
    Lista configuracion inicial y añadido el primer ejecutable

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
-
+#include <greetings.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[]){
-    printf("Holis, bienvenios al himalaia");
+int main(int argc, char *argv[])
+{
+    printf("Holis, bienvenios al himalaia \n");
+    greetings();
     return 0;
 }
-
-
-
